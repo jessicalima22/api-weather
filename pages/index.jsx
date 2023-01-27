8f5ed43ad81297d28142eeeb4485be7a7dbb1e02
jessicalima22 <jessica.lima22@hotmail.com>
2023-01-27 00:48:41 -0300

    icon import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faDroplet, faLocationDot, faMagnifyingGlass, faWind } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Home() {
   return (
@@ -27,54 +30,36 @@ export default function Home() {
               id="city-input" 
               />
               <button id="search">
-                <i classNames={styles.fa_solid}></i>
-
+                
+              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+              
               </button>
             </div>
           </div>
 
-        </div>
+          <div id="weather-data">
+            <h2><FontAwesomeIcon icon={faLocationDot}/> 
+              <span id="city">Maceió</span>
+              <img src="" id ="country" alt="flag country" />
+            </h2>
+            <p id="temperature"> <span>38</span>&deg;C</p>
+            <div id="description-container">
+              <p id="description">Nublado</p>
+              <img src="" alt="weather now" id="weather-icon" />
+            </div>
+            <div id="details-container">
+              <p id="umidity">
+              <FontAwesomeIcon icon={faDroplet}/>
+              <span>48%</span>
+              </p>
+              <p id="wind">
+              <FontAwesomeIcon icon={faWind}/>
+              <span>3km/h</span>
+              </p>
 
+            </div>
+          </div>
 
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
